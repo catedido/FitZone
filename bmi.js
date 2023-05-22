4a3aa4eae57dd234a193ec046d59f3ea0b3737e3
Gisela <gi-12@hotmail.com>
2023-05-22 18:51:32 +0100

    article 9 updated and its js file

--- bmi.js
+++ bmi.js
@@ -1,16 +1,27 @@
+function checkin() {
+    let checkBox = document.getElementById("myCheckBox");
+    let form = document.getElementById("form");
+    if (checkBox.checked === true){
+      form.style.display = "block";
+    } else {
+       form.style.display = "none";
+    }
+  }
 
 
-function calculatebmi(kg, cm) {
-    let result = (kg / cm**2) * 10000
-    alert(`Your BMI is ${result}`)
+
+document.getElementById('calculatebmi').addEventListener('click', function() {
+    let weigth = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    if (isNaN(height) || isNaN(weigth) === true ) {
+        document.getElementById("new-entry").innerText = "Please submit the correct values!";
+    } else if (height <= 0 || weigth <= 0) {
+        document.getElementById("new-entry").innerText = "Please submit the correct values!";
+    } else {
+    let result = (weigth / height**2) * 10000;
+    document.getElementById("new-entry").innerText = `Your BMI is ${result.toFixed(2)}`
+    }
 }
+);
 
-console.log(calculatebmi)
-
-
-const bmiButton = document.getElementById("bmi-button");
-bmiButton.addEventListener("click", function() {
-    let kg = prompt("What is your weight in Kilos?");
-    let cm = prompt("What is your height in centimeters?");
-    calculatebmi(kg,cm)
-})
