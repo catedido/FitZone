24ed0f06a9dc97d6af3db03a1ce7df33cfe45db6
Gisela <gi-12@hotmail.com>
2023-05-24 13:42:11 +0100

    bmi calculator button updated

--- bmi.js
+++ bmi.js
@@ -15,12 +15,12 @@ document.getElementById('calculatebmi').addEventListener('click', function() {
     let height = document.getElementById("height").value;
 
     if (isNaN(height) || isNaN(weigth) === true ) {
-        document.getElementById("new-entry").innerText = "Please submit the correct values!";
+        document.getElementById("new-entry").innerText = "❌ Please submit the correct values!";
     } else if (height <= 0 || weigth <= 0) {
-        document.getElementById("new-entry").innerText = "Please submit the correct values!";
+        document.getElementById("new-entry").innerText = "❌ Please submit the correct values!";
     } else {
     let result = (weigth / height**2) * 10000;
-    document.getElementById("new-entry").innerText = `Your BMI is ${result.toFixed(2)}`
+    document.getElementById("new-entry").innerText = `⚖️ Your BMI is ${result.toFixed(2)}`
     }
 }
 );
